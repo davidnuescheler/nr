236d1045109ab63778a2b293476a4e54e8ccbb98
rofe <github@rofe.com>
2021-02-08 20:39:07 +0100

    feat: sidekick plugins

---
+++ tools/sidekick/plugins.js
@@ -0,0 +1,32 @@
+// This file contains the nr-specific plugins for the sidekick.
+(() => {
+  const sk = window.hlx && window.hlx.sidekick ? window.hlx.sidekick : window.hlxSidekick;
+  if (typeof sk !== 'object') return;
+  sk.add({
+    id: 'preview',
+    override: true,
+    condition: (sidekick) => sidekick.isEditor() || (sidekick.isHelix() && sidekick.config.host),
+    button: {
+      text: '+',
+      action: async () => {
+        const { config, location } = sk;
+        sk.showModal('Please wait...');
+        let url;
+        if (sk.isEditor()) {
+          url = new URL('https://adobeioruntime.net/api/v1/web/helix/helix-services/gdrive-preview@0.0.1');
+          url.search = new URLSearchParams([
+            ['owner', config.owner],
+            ['repo', config.repo],
+            ['ref', config.ref || 'main'],
+            ['path', '/'],
+            ['lookup', location.href],
+          ]).toString();
+        } else {
+          const host = location.host === config.innerHost ? config.host : config.innerHost;
+          url = new URL(`https://${host}${location.pathname}`);
+        }
+        window.open(url.toString(), `hlx-sk-preview-${btoa(location.href)}`);
+      },
+    },
+  });
+})();
